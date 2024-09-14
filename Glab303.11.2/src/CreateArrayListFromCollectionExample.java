da99f4007bf77d05465f0dea53c376a557ce7a69
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-09-14 02:27:51 -0400

    SecondCommit

--- Glab303.11.2/src/CreateArrayListFromCollectionExample.java
+++ Glab303.11.2/src/CreateArrayListFromCollectionExample.java
@@ -1,2 +1,26 @@
-public class CreateArrayListFromCollectionExample {
+import java.util.ArrayList;
+
+public class CreateArrayListFromCollectionExample{
+
+    public static void main(String[] args) {
+        List<Integer> fistFivePrimeNumbers = new ArrayList<>();
+        fistFivePrimeNumbers.add(2);
+        fistFivePrimeNumbers.add(3);
+        fistFivePrimeNumbers.add(5);
+        fistFivePrimeNumbers.add(7);
+        fistFivePrimeNumbers.add(11);
+
+        List<Integer> firstTenPrimerNumbers = new ArrayList<>(fistFivePrimeNumbers);
+
+        List<Integer> nextFiveNumbers = new ArrayList<>();
+        nextFiveNumbers.add(13);
+        nextFiveNumbers.add(17);
+        nextFiveNumbers.add(19);
+        nextFiveNumbers.add(23);
+        nextFiveNumbers.add(29);
+
+        firstTenPrimerNumbers.addAll(nextFiveNumbers);
+
+        System.out.println(firstTenPrimerNumbers);
+    }
 }
--- Glab303.11.2/src/CreateArrayListFromCollectionExample2.java
+++ Glab303.11.2/src/CreateArrayListFromCollectionExample2.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateArrayListFromCollectionExample2 {
+    public static void main(String[] args) {
+        List<String> topCompanies = new ArrayList<>();
+
+        System.out.println("Is the topCompanies list empty? : " + topCompanies.isEmpty());
+
+        topCompanies.add("Google");
+        topCompanies.add("Apple");
+        topCompanies.add("Microsoft");
+        topCompanies.add("Amazon");
+        topCompanies.add("Facebook");
+
+        System.out.println("Here are the top " +topCompanies.size() + " companies in the world");
+
+        System.out.println(topCompanies);
+
+        String bestCompnay = topCompanies.get(0);
+        String secondBestCompany = topCompanies.get(1);
+        String lastCompany = topCompanies.get(topCompanies.size()-1);
+
+        System.out.println("Best Company: " + bestCompnay);
+
+        System.out.println("Second Best Copmany: " + secondBestCompany);
+
+        System.out.println("Last Company in the list: "+ lastCompany);
+
+        topCompanies.set(4, "Walmart");
+        System.out.println("Modified top companies list: " + topCompanies);
+    }
+
 }
